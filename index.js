f77a3383977d5411ac2075f3d082bea42611ea3a
Severyn Kozak <severyn.kozak@gmail.com>
2014-11-13 15:04:57 -0500

    CLI tool: better argument handling, JSON output.
    index.js
    	-Pipe the output stream of mapped objects to stdout in valid
    	JSON format.
    	-Add a `--help` argument, a better help/usage message.

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
+var jsonStream = require("JSONStream");
 var converter = require("./lib/converter");
 
 function handleUserArgs(args){
-	var helpMessage = "todo: help message.";
+	var helpMessage = [
+		"schema-mapper --help | RULES [...]",
+		"",
+		"\t--help: Print this message and exit.",
+		"\tRULES [...]: One or more JS/JSON files, each specifying how to",
+		"\t\tload a single dataset."
+	].join("\n");
 
 	if(args.length == 0){
 		console.error(helpMessage);
 		process.exit(1);
 	}
 
+	if(args[0] === "--help"){
+		console.log(helpMessage);
+		process.exit(0);
+	}
 	for(var ind = 0; ind < args.length; ind++){
-		var dataConverter = converter(require("./" + args[ind]));
+		var rulesFile = require("./" + args[ind]);
+		var dataConverter = converter(rulesFile);
+
 		if(dataConverter === null){
-			console.error("todo: error message.");
+			console.error(
+				"No Reader could be found for the `%s` format.",
+				rulesFile.reader.format
+			);
 			process.exit(1);
 		}
-		dataConverter.pipe(require("through")(function write(rec){
-			console.log(JSON.stringify(rec, undefined, 2));
-		}))
+
+		dataConverter
+			.pipe(jsonStream.stringify('{"objects":[\n', ",\n", "\n]}\n"))
+			.pipe(process.stdout);
 	}
 }
 
