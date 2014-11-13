3b6c34cbc43ce98e65b2f44db3e923ebd5210601
Severyn Kozak <severyn.kozak@gmail.com>
2014-11-13 15:54:53 -0500

    Document the remainder of the package.
    index.js, lib/reader.js
    	-Add documentation comments to file headers, and internal
    	functions.

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
+/**
+ * A CLI tool that wraps the `schema-mapper` library.
+ */
+
 var jsonStream = require("JSONStream");
 var converter = require("./lib/converter");
 
+/**
+ * Handle command-line arguments.
+ *
+ * If a `rules` file is supplied, attempt reading it and pipe the dataset's
+ * remapped objects to `stdout` in valid JSON format. May `exit()`, if
+ * incorrect arguments are supplied.
+ *
+ * @param {array of string} args `process.argv.slice(2)`
+ */
 function handleUserArgs(args){
 	var helpMessage = [
 		"schema-mapper --help | RULES [...]",
@@ -10,7 +23,7 @@ function handleUserArgs(args){
 		"\t\tload a single dataset."
 	].join("\n");
 
-	if(args.length == 0){
+	if(args.length === 0){
 		console.error(helpMessage);
 		process.exit(1);
 	}
@@ -19,6 +32,7 @@ function handleUserArgs(args){
 		console.log(helpMessage);
 		process.exit(0);
 	}
+
 	for(var ind = 0; ind < args.length; ind++){
 		var rulesFile = require("./" + args[ind]);
 		var dataConverter = converter(rulesFile);
