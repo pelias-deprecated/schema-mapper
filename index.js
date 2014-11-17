27cce596bd610fcb6fcc40f1be6ee12bb1d20c26
Severyn Kozak <severyn.kozak@gmail.com>
2014-11-17 11:20:46 -0500

    Add flag to CLI tool to examine single objects.
    index.js
    	-Add an `--examine` flag to the CLI tool that directs it to
    	print out a single record read in from a target dataset. Useful
    	for getting the dataset's original schema.
    	-Update `helpMessage` accordingly.
    	-Add an abbreviation `-h` for `--help`, and `-e` for
    	`--examine`.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 var jsonStream = require("JSONStream");
 var converter = require("./lib/converter");
+var reader = require("./lib/reader");
 
 /**
  * Handle command-line arguments.
@@ -16,9 +17,12 @@ var converter = require("./lib/converter");
  */
 function handleUserArgs(args){
 	var helpMessage = [
-		"schema-mapper --help | RULES [...]",
+		"schema-mapper --help | --examine RULES | RULES [...]",
 		"",
-		"\t--help: Print this message and exit.",
+		"\t--help, -h: Print this message and exit.",
+		"\t--examine, -e RULES: Read and print one record from the dataset",
+		"\t\tpointed to by the argument RULES file. Useful for examining its",
+		"\t\toriginal schema.",
 		"\tRULES [...]: One or more JS/JSON files, each specifying how to",
 		"\t\tload a single dataset."
 	].join("\n");
@@ -28,16 +32,22 @@ function handleUserArgs(args){
 		process.exit(1);
 	}
 
-	if(args[0] === "--help"){
+	if(args[0] === "--help" || args[0] === "-h"){
 		console.log(helpMessage);
-		process.exit(0);
 	}
 
-	for(var ind = 0; ind < args.length; ind++){
-		var rulesFile = require("./" + args[ind]);
-		var dataConverter = converter(rulesFile);
+	else if(args[0] === "--examine" || args[0] === "-e"){
+		if(args.length !== 2){
+			console.error(
+				"Insufficient arguments: missing a rules file.\n\n",
+				helpMessage
+			);
+			process.exit(1);
+		}
 
-		if(dataConverter === null){
+		var rulesFile = require("./" + args[1]);
+		var dataReader = reader(rulesFile.reader);
+		if(dataReader === null){
 			console.error(
 				"No Reader could be found for the `%s` format.",
 				rulesFile.reader.format
@@ -45,9 +55,29 @@ function handleUserArgs(args){
 			process.exit(1);
 		}
 
-		dataConverter
-			.pipe(jsonStream.stringify('{"objects":[\n', ",\n", "\n]}\n"))
-			.pipe(process.stdout);
+		dataReader.once("readable", function (){
+			console.log(JSON.stringify(dataReader.read(), undefined, 4));
+			process.exit(0);
+		});
+	}
+
+	else {
+		for(var ind = 0; ind < args.length; ind++){
+			var rulesFile = require("./" + args[ind]);
+			var dataConverter = converter(rulesFile);
+
+			if(dataConverter === null){
+				console.error(
+					"No Reader could be found for the `%s` format.",
+					rulesFile.reader.format
+				);
+				process.exit(1);
+			}
+
+			dataConverter
+				.pipe(jsonStream.stringify('{"objects":[\n', ",\n", "\n]}\n"))
+				.pipe(process.stdout);
+		}
 	}
 }
 
