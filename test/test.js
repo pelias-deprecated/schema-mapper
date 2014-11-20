c94b98800b9fab760fea0f5b79330c11b86fb26f
Severyn Kozak <severyn.kozak@gmail.com>
2014-11-20 14:28:01 -0500

    Begin writing unit tests: add main entry point.
    test/test.js
    	-Add the main entry point for all unit-tests.
    
    package.json
    	-Add test dependencies.

---
+++ test/test.js
@@ -0,0 +1,47 @@
+/**
+ * @file The main entry point for the schema-mapper package's unit tests.
+ *      Imports and runs all of the sibling files (assumed to be modules that
+ *      export objects containing `tape` test functions) in this file's
+ *      directory.
+ */
+
+"use strict";
+
+var fs = require("fs");
+var tape = require("tape");
+var util = require("util");
+
+/**
+ * Execute all functions in the object exported by a module.
+ *
+ * @param {string} testFilePath The path to the module. Will be imported, and
+ *      each of its exported functions will be executed by `tape()`.
+ */
+function runTestFile(testFilePath){
+	var testModule = require(testFilePath);
+	for(var test in testModule){
+		if(testModule.hasOwnProperty(test)){
+			var testName = util.format(
+				"File: %s, Function: %s", testFilePath, test
+			);
+			tape(testName, testModule[test]);
+		}
+	}
+}
+
+/**
+ * Run all modules in this module's directory with `runTestFile()`.
+ */
+!function runTestFiles(){
+	fs.readdir(__dirname, function readFilesCallback(err, files){
+		if(err){
+			console.err(err);
+			process.exit(1);
+		}
+
+		files.splice(files.indexOf(__filename));
+		for(var ind = 0; ind < files.length; ind++){
+			runTestFile("./" + files[ind]);
+		}
+	})
+}();
