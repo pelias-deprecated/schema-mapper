ce2698a8b41346e448f6d0c778e01ebd5f8a1dbe
Severyn Kozak <severyn.kozak@gmail.com>
2014-11-20 15:18:57 -0500

    Define a test for createMapper() String fields.
    test/mapper.js
    	-Define `createMapperFields()`, which tests whether
    	`createMapper()` properly remaps String fields.

--- test/mapper.js
+++ test/mapper.js
@@ -8,6 +8,7 @@
 var util = require("util");
 var tape = require("tape");
 var mapper = require("../lib/mapper");
+var readableStream = require("readable-stream");
 
 var tests = {};
 
@@ -50,14 +51,59 @@ tests.getPropVal = function getPropVal(test){
 			var expectedValue = testCases[testCase];
 			var actualValue = mapper.getPropVal(object, path)
 
-			var errorMsg = util.format(
+			var msg = util.format(
 				"Succeeded for path `%s` in `%s`. Expected: `%s`. Got: `%s`.",
-				path, JSON.stringify(object, undefined, 4), expectedValue,
+				path, JSON.stringify(object), expectedValue,
 				actualValue
 			);
-			test.deepEqual(actualValue, expectedValue, errorMsg);
+			test.deepEqual(actualValue, expectedValue, msg);
 		}
 	}
 };
+
+/**
+ * Test whether the Transform stream created by `createMapper()` properly
+ * remaps String fields, by writing a number of dummy objects through a simple
+ * mapper.
+ */
+tests.createMapperFields = function createMapper(test){
+	var input = [
+		{a: 1, b: 2},
+		{a: 3, b: 4},
+		{a: 5, b: 6},
+	];
+
+	Error.stackTraceLimit = Infinity;
+	var mapperConfig = {
+		fields: {
+			col1: "a",
+			col2: "b"
+		}
+	}
+
+	var expectedOutput = [
+		{col1: 1, col2: 2},
+		{col1: 3, col2: 4},
+		{col1: 5, col2: 6},
+	];
+
+	test.plan(expectedOutput.length);
+
+	var mapperStream = mapper.createMapper(mapperConfig);
+	var testStream = new readableStream.Writable({objectMode: true});
+	testStream._write = function write(data, enc, next){
+		var msg = util.format(
+			"Remapped object `%s` matches expected.",
+			JSON.stringify(expectedOutput[ind])
+		);
+		test.deepEqual(data, expectedOutput.shift(), msg);
+		next();
+	};
+	mapperStream.pipe(testStream);
+
+	for(var ind = 0; ind < input.length; ind++){
+		mapperStream.write(input[ind]);
+	}
+}
 
 module.exports = tests;
